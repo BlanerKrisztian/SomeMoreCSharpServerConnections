b982e48e97b19598f8de90a690d5cda509feb001
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-23 08:45:41 +0100

    Added server post delete, and started working on Load Post Delete on the website

---
+++ public/script.js
@@ -0,0 +1,3 @@
+async function Load(){
+    response = await fetch("/factions")
+}
--- routes.js
+++ routes.js
@@ -1,10 +1,23 @@
 const express = require("express")
 const router = express.Router()
 
-const factions = ["Tyrranids", "Automatons", "Squith", "GLA", "USA","Terran","Greenskins","Chaos","Nurgle","GDI","NOD","Danube Federation","Empire"]
+const factions = ["Tyrranids", "Automatons", "Squith", "GLA", "USA","Terran","Greenskins","Chaos","Nurgle","GDI","NOD","Danube Federation","Empire", "Deepwater Guard"]
 
 router.get("/factions",(req,res) => {
-    res.status(200).json({Message:"All factions I thought of"}).end()
+    res.status(200).json({Message:"All factions I thought of",Factions:factions}).end()
+})
+
+router.post("/faction",(req,res) => {
+    const newFaction = req.body.newFaction
+    factions.push(newFaction)
+    console.log(`Added new faction: ${newFaction}`)
+    res.status(201).json({Message:`${newFaction} successfully added`}).end()
+})
+
+router.delete("/faction",(req,res) => {
+    console.log(`Deleted a faction`)
+    factions.pop()
+    res.status(200).json({Message:`successfully Deleted a faction`}).end()
 })
 
 module.exports = router

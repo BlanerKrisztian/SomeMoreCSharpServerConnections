7de43577ce24ffe86b6fc67309a9f8f89307e903
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-23 09:32:48 +0100

    Added website server interaction

--- public/script.js
+++ public/script.js
@@ -1,3 +1,54 @@
+const factionList = document.getElementById("factionList")
+const addfaction = document.getElementById("addFactionInput")
+
 async function Load(){
+    factionList.innerHTML = "Fetching Factions"
     response = await fetch("/factions")
+    result = await response.json()
+    console.log(result)
+
+    const factions = result.factions
+    if (!response.ok) {
+        factionList.innerHTML = "Failed to load"
+        return
+    }
+    factionList.innerHTML = ""
+    for (item of factions){
+        console.log(item)
+        const li = document.createElement("li")
+        li.innerHTML = item
+        factionList.appendChild(li)
+    }
 }
+
+async function Create(){
+    console.log(addfaction.value)
+    response = await fetch("/faction", {
+        method:"POST",
+        headers:{"Content-Type":"Application/JSON"},
+        body: JSON.stringify({newFaction :addfaction.value})
+    })
+    
+    result = await response.json()
+    console.log(result)
+    if (response.ok){
+        Load()
+    }
+}
+
+async function Delete(){
+    response = await fetch("/faction",{
+        method:"DELETE"
+    })
+    result = await response.json()
+    console.log(result)
+    if (response.ok) {
+        
+        Load()
+        alert(result.Message)
+        return
+    }else{
+        alert("delete failed")
+    }
+    }
+Load()
--- routes.js
+++ routes.js
@@ -4,14 +4,14 @@ const router = express.Router()
 const factions = ["Tyrranids", "Automatons", "Squith", "GLA", "USA","Terran","Greenskins","Chaos","Nurgle","GDI","NOD","Danube Federation","Empire", "Deepwater Guard"]
 
 router.get("/factions",(req,res) => {
-    res.status(200).json({Message:"All factions I thought of",Factions:factions}).end()
+    res.status(200).json({Message:"All factions I thought of",factions:factions}).end()
 })
 
 router.post("/faction",(req,res) => {
     const newFaction = req.body.newFaction
     factions.push(newFaction)
     console.log(`Added new faction: ${newFaction}`)
-    res.status(201).json({Message:`${newFaction} successfully added`}).end()
+    res.status(200).json({Message:`${newFaction} successfully added`}).end()
 })
 
 router.delete("/faction",(req,res) => {

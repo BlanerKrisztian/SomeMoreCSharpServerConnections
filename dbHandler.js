7238642059c147151153d20568a2024639327097
Blaner Krisztián <barkiza760bc@gmail.com>
2026-03-30 09:08:22 +0200

    Finished table weathertype

--- dbHandler.js
+++ dbHandler.js
@@ -12,5 +12,7 @@ const dbHandler = new Sequelize("weather","root","",{
 
 const table = dbHandler.define("weathertype",{
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
-    name: {type: DataTypes.STRING, allowNull: false}
+    name: {type: DataTypes.STRING, allowNull: false},
+    intensity: {type: DataTypes.DOUBLE, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: true}
 })
